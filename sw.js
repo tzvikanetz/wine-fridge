44921d95ff9b648cd4fd032c7228f49bdc6daa67
tzvikanetz <tzvikanetz@gmail.com>
2026-04-08 13:05:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'wine-fridge-v1';
+const ASSETS = [
+  '/wine-fridge/',
+  '/wine-fridge/index.html',
+  '/wine-fridge/icon-192.png',
+  '/wine-fridge/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
